6f45724cc8388241af57968259024c7bb4a6303b
JacksonSmith43 <jessica444@gmx.at>
2025-05-24 15:54:53 +0200

    19. Preparing User Data (To Output Dynamic Content).

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,27 @@
+export const DUMMY_USERS = [
+  {
+    id: 'ui',
+    name: 'Galadriel',
+    avatar: 'galadriel.avif',
+  },
+  {
+    id: 'ui2',
+    name: 'Saphira',
+    avatar: 'saphira.jpg',
+  },
+  {
+    id: 'ui3',
+    name: 'Dobby',
+    avatar: 'dobby.jpg',
+  },
+  {
+    id: 'ui4',
+    name: 'Poirot',
+    avatar: 'poirot.webp',
+  },
+  {
+    id: 'ui5',
+    name: 'Yoda',
+    avatar: 'yoda.webp',
+  },
+];
