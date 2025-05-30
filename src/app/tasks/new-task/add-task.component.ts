1f2a51342b6eb00a50ca384bc4d3fb98f03cb0b3
JacksonSmith43 <jessica444@gmx.at>
2025-05-30 10:56:32 +0200

    50. Creating & Conditionally Rendering Another Component.

---
+++ src/app/tasks/new-task/add-task.component.ts
@@ -0,0 +1,12 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task',
+  imports: [],
+  templateUrl: './add-task.component.html',
+  styleUrl: './add-task.component.css'
+})
+
+export class AddTaskComponent {
+
+}
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { AddTaskComponent } from './new-task/add-task.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, AddTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -11,6 +12,7 @@ import { TaskComponent } from "./task/task.component";
 export class TasksComponent {
   @Input() name?: string; // Union Type can also be used that would replace the ?. For example: name: string | undefined
   @Input({ required: true }) userId!: string;
+  isAddingTask = false;
 
   tasks = [
     {
@@ -41,5 +43,18 @@ export class TasksComponent {
 
   onCompleteTaskParent(id: string) { // Added Parent to the name, to better understand the Ablauf of Methods call, seeing as task also had this name (onCompleteTask).
     this.tasks = this.tasks.filter((task) => task.id !== id); // Due to another array being created, the original array is not modified, but a new one is created, which causes a re-render of the component and when clicking Complete the task is removed from the list. task.id !== id is the Bedienung. If it is true, the task is not included in the new array, if it is false, the task is included in the new array. 
+  }
+
+  onAddTask() {
+
+    /* this.tasks.push({
+       id: 't' + (this.tasks.length + 1),
+       userId: this.userId,
+       title: 'New Task',
+       summary: 'Summary of new task',
+       dueDate: new Date().toISOString().split('T')[0]
+     });*/
+
+    this.isAddingTask = true;
   }
 }
